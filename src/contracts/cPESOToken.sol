bbb206658956846824371431c0d531b229b94b28
Brindrajsinh-Chauhan <91854080+Brindrajsinh-Chauhan@users.noreply.github.com>
2021-10-23 20:08:02 -0400

    Add files via upload

--- src/contracts/cPESOToken.sol
+++ src/contracts/cPESOToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-contract USDToken {
+contract cPESOToken {
     string  public name = "cPESO Token";
     string  public symbol = "cPESO";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
